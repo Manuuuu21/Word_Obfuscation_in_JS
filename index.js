73fd35137e1e47721ff716c42dbcb6665a343ed6
Manuuuu21 <151635459+Manuuuu21@users.noreply.github.com>
2023-12-14 21:42:19 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ _0x636f6e76657274546f48657861646563696d616c=()=> {
 	var _0x776f7264 = document.getElementById("_0x696e705f6f62667573636174696f6e").value;
 	var _0x686578576f7264 = "";
 
-	for (let i = 0; i < _0x776f7264.length; i++) {
-		    let _0x68657861646563696d616c = _0x776f7264.charCodeAt(i).toString(16);
+	for (let _0x69 = 0; _0x69 < _0x776f7264.length; _0x69++) {
+		    let _0x68657861646563696d616c = _0x776f7264.charCodeAt(_0x69).toString(16);
 		    _0x686578576f7264 += _0x68657861646563696d616c;
 	}
-
-	console.log(`Hexadecimal representation of: _0x${_0x686578576f7264}`);
 	document.getElementById("_0x6f62667573636174696f6e5f76616c7565").innerHTML = `_0x` + _0x686578576f7264;
 }
-
 
 _0x636f6e76657274546865486578546f4f726967696e616c=()=> {
 	let _0x686578537472696e67 = document.getElementById("_0x646563727970745f686578615f76616c").value;
@@ -21,12 +18,10 @@ _0x636f6e76657274546865486578546f4f726967696e616c=()=> {
 		_0x686578537472696e67 = _0x686578537472696e67.slice(3);
 	}
 
-	for (let i = 0; i < _0x686578537472696e67.length; i += 2) {
-		let _0x686578 = _0x686578537472696e67.substring(i, i+2);
+	for (let _0x69 = 0; _0x69 < _0x686578537472696e67.length; _0x69 += 2) {
+		let _0x686578 = _0x686578537472696e67.substring(_0x69, _0x69+2);
 		let _0x63686172 = String.fromCharCode(parseInt(_0x686578, 16));
 		_0x737472 += _0x63686172;
 	}
-
-	console.log(`English representation of Hexadecimal: ${_0x737472}`);
 	document.getElementById("_0x646563727970745f68657861").innerHTML = _0x737472;
 }
