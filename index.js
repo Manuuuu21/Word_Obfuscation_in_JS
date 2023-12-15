cb4f1bb093537798e3d528828c0ae258485f7a64
Manuuuu21 <151635459+Manuuuu21@users.noreply.github.com>
2023-12-16 02:45:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-_0x636f6e76657274546f48657861646563696d616c=()=> {
-	let _0x776f7264 = document.getElementById("_0x696e705f6f62667573636174696f6e").value;
-	let _0x686578576f7264 = "";
+// Function to convert a string to hexadecimal
+convertToHexadecimal = () => {
+    // Get the input string from the HTML element with id "input_obfuscation"
+    let word = document.getElementById("input_obfuscation").value;
+    let hexWord = "";
 
-	for (let _0x69 = 0; _0x69 < _0x776f7264.length; _0x69++) {
-		    let _0x68657861646563696d616c = _0x776f7264.charCodeAt(_0x69).toString(16);
-		    _0x686578576f7264 += _0x68657861646563696d616c;
-	}
-	document.getElementById("_0x6f62667573636174696f6e5f76616c7565").innerHTML = `_0x` + _0x686578576f7264;
+    // Loop through each character in the string
+    for (let i = 0; i < word.length; i++) {
+        // Convert each character to hexadecimal and append to hexWord
+        let hexadecimal = word.charCodeAt(i).toString(16);
+        hexWord += hexadecimal;
+    }
+
+    // Display the hexadecimal string in the HTML element with id "obfuscation_result"
+    document.getElementById("obfuscation_result").innerHTML = `_0x` + hexWord;
 }
 
-_0x636f6e76657274546865486578546f4f726967696e616c=()=> {
-	let _0x686578537472696e67 = document.getElementById("_0x646563727970745f686578615f76616c").value;
-	let _0x737472 = '';
+// Function to convert a hexadecimal string back to the original string
+convertHexToOriginal = () => {
+    // Get the hexadecimal string from the HTML element with id "decrypt_hex"
+    let hexString = document.getElementById("decrypt_hex").value;
+    let str = '';
 
-	if (_0x686578537472696e67.startsWith('_0x')) {
-		_0x686578537472696e67 = _0x686578537472696e67.slice(3);
-	}
+    // Check if the hexadecimal string starts with "_0x" and remove it
+    if (hexString.startsWith('_0x')) {
+        hexString = hexString.slice(3);
+    }
 
-	for (let _0x69 = 0; _0x69 < _0x686578537472696e67.length; _0x69 += 2) {
-		let _0x686578 = _0x686578537472696e67.substring(_0x69, _0x69+2);
-		let _0x63686172 = String.fromCharCode(parseInt(_0x686578, 16));
-		_0x737472 += _0x63686172;
-	}
-	document.getElementById("_0x646563727970745f68657861").innerHTML = _0x737472;
+    // Loop through each pair of characters in the hexadecimal string
+    for (let i = 0; i < hexString.length; i += 2) {
+        // Convert each pair of hexadecimal characters back to the original character
+        let hex = hexString.substring(i, i+2);
+        let char = String.fromCharCode(parseInt(hex, 16));
+        str += char;
+    }
+
+    // Display the original string in the HTML element with id "decrypt_hex_result"
+    document.getElementById("decrypt_hex_result").innerHTML = str;
 }
